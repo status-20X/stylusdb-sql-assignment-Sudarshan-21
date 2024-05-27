58576a6691c194ae22c780cad1ba266efaed9edb
Sudarshan-21 <slbondge@gmail.com>
2024-05-27 15:10:28 +0530

    final test

--- src/csvReader.js
+++ src/csvReader.js
@@ -1,6 +1,7 @@
 // src/csvReader.js
 
 const fs = require("fs");
+const { parse } = require("json2csv");
 const csv = require("csv-parser");
 
 function readCSV(filePath) {
@@ -19,4 +20,29 @@ function readCSV(filePath) {
   });
 }
 
-module.exports = readCSV;
+async function writeCSV(filename, data) {
+  try {
+    const csvData = parse(data);
+    fs.writeFileSync(filename, csvData);
+  } catch (error) {
+    throw error;
+  }
+}
+function readCSVForHLL(filePath, bitSampleSize = 12, digestSize = 128) {
+  const results = [];
+  var h = hll({ bitSampleSize: bitSampleSize, digestSize: digestSize });
+
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => h.insert(JSON.stringify(data)))
+      .on("end", () => {
+        resolve(h);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+}
+
+module.exports = { readCSV, writeCSV };
